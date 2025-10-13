f2ad8a440c23dddb40ec98eb1a00d82f93635e9d
Maciej Grześkowiak <maciej.grzes07@gmail.com>
2025-10-13 17:21:13 +0200

    Zadanie 6

--- LAB02/zadania.cpp
+++ LAB02/zadania.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
 using namespace std;
 
+/*
+    Zadanie1:
+Output:
+
+5
+6
+7
+
+5
+6
+
+0
+2
+4
+6
+8
+10
+
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+0
+
+Code:
+
 int main() {
     for (int i = 5; i <= 7; i++) {
        cout << i << endl;
     }
 
-    cout << endl;
+cout << endl;
 
     for (int i = 5; i < 7; i++) {
        cout << i << endl;
@@ -26,3 +58,26 @@ int main() {
 
     return 0;
 }
+*/
+
+/*
+    Zadanie6:
+
+Code:
+
+int main() { 
+    int fahr, celsius; 
+    int start, limit, krok; 
+
+    start = 0; 
+
+    cin >> limit;
+    cin >> krok;
+
+    fahr = start; 
+    for(fahr = 0.0; fahr <= limit; fahr = fahr + krok) { 
+        float celsius = (5.0/9.0)*(fahr - 32.0); 
+        cout << fahr << "\t" << celsius << endl;
+    }  
+}
+*/
