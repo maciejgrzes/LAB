41c21fc9e63d0ea927f19a0a0b8f6880c161d11a
Maciej Grześkowiak <maciej.grzes07@gmail.com>
2025-10-13 16:16:31 +0200

    Zadanie 2

--- LAB01/zadania.cpp
+++ LAB01/zadania.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 
-/*G
-    X     X    XXXXXX 
-    XX   XX   X      X   
-    X X X X   X       
-    X  X  X   X   XXX    
-    X     X   X      X
-    X     X    XXXXXX
-*/
+/*
+    Zadanie 1:
+Output:
+ 
+X     X    XXXXXX 
+XX   XX   X      X   
+X X X X   X       
+X  X  X   X   XXX    
+X     X   X      X
+X     X    XXXXXX
+
+
 int main() {
     cout << "X     X    XXXXXX " << endl;
     cout << "XX   XX   X      X" << endl;
@@ -16,6 +20,27 @@ int main() {
     cout << "X  X  X   X   XXX " << endl;
     cout << "X     X   X      X" << endl;
     cout << "X     X    XXXXXX " << endl;
+
+    return 0;
+}
+
+*/
+
+/*
+    *   
+    *   
+ *******
+    *
+   * *  
+  *   * 
+*/
+int main () {
+    cout << "    *   " << endl;
+    cout << "    *   " << endl;
+    cout << " *******" << endl;
+    cout << "    *   " << endl;
+    cout << "   * *  " << endl;
+    cout << "  *   * " << endl;
 
     return 0;
 }
