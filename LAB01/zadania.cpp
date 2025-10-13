76028267368d79b0772ada15050be94300c7c546
Maciej Grześkowiak <maciej.grzes07@gmail.com>
2025-10-13 16:28:12 +0200

    Zadanie 4

--- LAB01/zadania.cpp
+++ LAB01/zadania.cpp
@@ -13,6 +13,8 @@ X     X   X      X
 X     X    XXXXXX
 
 
+Code:
+
 int main() {
     cout << "X     X    XXXXXX " << endl;
     cout << "XX   XX   X      X" << endl;
@@ -27,13 +29,19 @@ int main() {
 */
 
 /*
+    Zadanie2:
+Output:
+
+
     *   
     *   
  *******
     *
    * *  
   *   * 
-*/
+
+Code:
+
 int main () {
     cout << "    *   " << endl;
     cout << "    *   " << endl;
@@ -44,3 +52,50 @@ int main () {
 
     return 0;
 }
+*/
+
+
+/*
+    Zadanie3:
+Output:
+Nr     Imie    Nazwisko
+1      Maciej  Grzeskowiak
+2      Maciej  Grzeskowiak
+3      Maciej  Grzeskowiak
+
+
+Code:
+
+int main() {
+    cout << "Nr     Imie    Nazwisko" << endl;
+    cout << "1      Maciej  Grzeskowiak" << endl;
+    cout << "2      Maciej  Grzeskowiak" << endl;
+    cout << "3      Maciej  Grzeskowiak" << endl;
+    
+
+    return 0;
+}
+*/
+
+
+/*
+    Zadanie4:
+
+
+
+int main() {
+    int a;
+    int b;
+    cin >> a;
+    cout << endl;
+    cin >> b;
+
+    cout << a+b << " " << a*b << endl;
+
+    return 0;
+}
+*/
+
+
+
+
