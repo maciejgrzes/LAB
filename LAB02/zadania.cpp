72220567b7b5b8b03f1c52978162d95ebbcc7fe2
Maciej Grześkowiak <maciej.grzes07@gmail.com>
2025-10-13 17:06:24 +0200

    Zadanie 1

---
+++ LAB02/zadania.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 5; i <= 7; i++) {
+       cout << i << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 5; i < 7; i++) {
+       cout << i << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i <= 10; i += 2) {
+       cout << i << endl;
+    }
+
+    cout << endl;
+    
+    for (int i = 10; i >= 0; i--) {
+       cout << i << endl;
+    }
+
+    return 0;
+}
