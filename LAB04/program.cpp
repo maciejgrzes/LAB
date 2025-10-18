77782bf158a2fcdf24b1f8f27d41193e733a9c7b
Maciej Grześkowiak <maciej.grzes07@gmail.com>
2025-10-18 21:31:43 +0200

    Replaced 6 functions with one that also takes an option variable, also added the showMenu procedure

--- LAB04/program.cpp
+++ LAB04/program.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-using std::cout;
-using std::endl;
+using namespace std;
 
+/*
 double FtoC (double F) {return (F - 32.0) * 5.0/9.0;}
 
 double FtoK (double F) {return (F + 459.67) * 5.0/9.0;}
@@ -13,19 +13,41 @@ double CtoK (double C) {return C + 273.15;}
 double KtoC (double K) {return K - 273.15;}
 
 double KtoF (double K) {return K * 9.0/5.0 - 459.67;}
+*/
 
+void showMenu() {
+    cout << "1 - przelicz Fahr -> Celsius" << endl;
+    cout << "2 - przelicz Fahr -> Kelwin" << endl;
+    cout << "3 - przelicz Celsius -> Fah" << endl;
+    cout << "4 - przelicz Celsius -> Kelwin" << endl;
+    cout << "5 - przelicz Kelwin -> Celsius" << endl;
+    cout << "6 - przelicz Kelwin -> Fahr" << endl;
+    cout << "7 - zakończ działanie programu" << endl;
+}
+
+double convert(int i, double n) {
+    switch(i) {
+    case 1: // FtoC
+        return (n - 32.0) * 5.0/9.0;
+    case 2: // FtoK
+        return (n + 459.67) * 5.0/9.0;
+    case 3: // CtoF
+        return n * 9.0/5.0 + 32.0;
+    case 4: // CtoK
+        return n + 273.15;
+    case 5: // KtoC
+        return n - 273.15;
+    case 6: // KtoF
+        return n * 9.0/5.0 - 459.67;
+    default:
+        return 0;
+    }
+}
 
 
 int main() {
-    int n = 25;
-
-    cout << FtoC(n) << endl;
-    cout << FtoK(n) << endl;
-    cout << CtoF(n) << endl;
-    cout << CtoK(n) << endl;
-    cout << KtoC(n) << endl;
-    cout << KtoF(n) << endl;
-        
+    showMenu();
+    
 
     return 0;
 }
