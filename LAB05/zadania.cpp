a1212aafad66f717c99d1bd569ab506bdbd009d2
Maciej Grześkowiak <maciej.grzes07@gmail.com>
2025-10-23 18:27:55 +0200

    Zadanie 2

--- LAB05/zadania.cpp
+++ LAB05/zadania.cpp
@@ -5,7 +5,7 @@ using namespace std;
     Zadanie1:
 
 Code:
-*/
+
 
 int main() {
     int table[10];
@@ -17,6 +17,37 @@ int main() {
 
     for (int i = 9; i >= 0; i--) {
         cout << table[i] << endl;
+    }
+
+    return 0;
+}
+*/
+
+
+/*
+    Zadanie2:
+
+Code:
+*/
+
+void fibonacci(int fib[],int size) {
+    for (int i = 2; i <= size; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+}
+
+int main() {
+    int size;
+    cin >> size;
+
+    int fib[size];
+    fib[0] = 0;
+    fib[1] = 1;
+
+    fibonacci(fib, size);
+
+    for (int num : fib) {
+        cout << num << endl;
     }
 
     return 0;
