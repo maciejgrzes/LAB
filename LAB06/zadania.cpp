3870817c1396ce87d8da31cc342a1f141fe06e3a
Maciej Grześkowiak <maciej.grzes07@gmail.com>
2025-11-10 19:12:06 +0100

    Zadanie 2

--- LAB06/zadania.cpp
+++ LAB06/zadania.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+/*
+    Zadanie1:
+
+Code:
+
+
 const int a = 20;
 const int b = 10;
 
@@ -17,5 +23,52 @@ int main () {
 
     cout << sum(a, b) << endl;
 
+    return 0;
+}
+*/ 
+
+/*
+    zadanie2:
+
+Code:
+*/
+
+int tab[10] = {0};
+int rozmiar = 10;
+
+void printTab() {
+    for (int num : tab) {
+        cout << num << ' ';
+    }
+    cout << endl;
+}
+
+void deleteIndex(int index) {
+    if (index > rozmiar-1) {
+        cout << "Out of range!";
+        return;
+    }
+    for (int i = index; i < rozmiar; i++) {
+        tab[i] = tab[i+1];
+    }
+    tab[rozmiar-1] = 0;
+    rozmiar--;
+}
+
+int main() {
+    int v;
+    for (int i = 0; i < rozmiar; i++) {
+        cin >> v;
+        tab[i] = v;
+    }
+
+
+    printTab();
+
+    int index;
+    cin >> index;
+    deleteIndex(index);
+
+    printTab();
     return 0;
 }
