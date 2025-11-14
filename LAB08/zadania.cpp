28009a9aea2dc8f1b343473c0623d706db29bc49
Maciej Grześkowiak <maciej.grzes07@gmail.com>
2025-11-15 00:29:19 +0100

    Zadanie 2 i 2.1

--- LAB08/zadania.cpp
+++ LAB08/zadania.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+/*
+    Zadanie1:
+
+Code:
+
 
 int main() {
     int x;
@@ -12,3 +17,57 @@ int main() {
     
     return 0;
 }
+*/
+
+/*
+    Zadanie2:
+
+Code:
+
+int main() {
+    int tab[10];
+    int *ptr;
+
+    ptr = &tab[0];
+
+    for (int num : tab) {
+        cout << num << ' ';
+    } cout << endl;
+    
+    ptr += 7;
+    *ptr = 77;
+
+    for (int num : tab) {
+        cout << num << ' ';
+    } cout << endl;
+
+    return 0;
+}
+*/
+
+/*
+    Zadanie2.1;
+Code:
+
+int main() {
+    int tab[10] = {0};
+    int *ptr;
+
+    ptr = &tab[0];
+
+    for (int num : tab) {
+        cout << num << ' ';
+    } cout << endl;
+
+    for (int i = 0; i < 10; i++) {
+        *ptr = 9;
+        ptr++;
+    }
+
+    for (int num : tab) {
+        cout << num << ' ';
+    } cout << endl;
+
+    return 0;
+}
+*/
