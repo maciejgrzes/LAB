34dd16e85f42e96f0df97715d6caa3edfc6d74dd
Maciej Grześkowiak <maciej.grzes07@gmail.com>
2025-11-18 15:38:29 +0100

    Zadanie 3

--- LAB08/zadania.cpp
+++ LAB08/zadania.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <random>
 using namespace std;
 
 /*
@@ -67,6 +68,65 @@ int main() {
     for (int num : tab) {
         cout << num << ' ';
     } cout << endl;
+
+    return 0;
+}
+*/
+
+/*
+    Zadanie3:
+
+Code:
+
+
+int max(int *arr, int size) {
+    int res = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (res < arr[i]) {
+            res = arr[i];
+        }
+    }
+    return res;
+}
+
+int min(int *arr, int size) {
+    int res = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (res > arr[i]) {
+            res = arr[i];
+        }
+    }
+    return res;
+}
+
+
+int main() {
+    random_device rd;
+    mt19937 gen(rd());
+
+    uniform_int_distribution<int> num(0,10);
+
+
+    int size;
+    cin >> size;
+
+    int *arr = new int[size];
+    
+    for (int i = 0; i < size; i++) {
+        arr[i] = num(gen);
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << ' ';
+    } cout << endl;
+
+    int x, y;
+    cin >> x >> y;
+
+
+    cout << arr[x] + arr[y] << endl;
+    cout << arr[x] * arr[y] << endl;
+    cout << max(arr, size) - min(arr, size) << endl;
 
     return 0;
 }
