ecb729fe7314462b555271fbcd1c8488c8dd49d8
Maciej Grześkowiak <maciej.grzes07@gmail.com>
2025-11-18 16:11:10 +0100

    Zadanie 1

---
+++ LAB09/zadania.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+using namespace std;
+
+/*
+    Zadanie1:
+
+Code:
+
+
+struct Point {
+    int x;
+    int y;
+};
+
+int main() {
+    Point point = {2, 5};
+
+    cout << point.x << endl; 
+    cout << point.y << endl;
+
+    return 0;
+}
+*/
