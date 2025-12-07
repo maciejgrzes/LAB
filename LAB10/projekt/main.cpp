7c9bdbfe7751b31db64d6fa5ea16d2dc714e4f9f
Maciej Grześkowiak <maciej.grzes07@gmail.com>
2025-12-07 01:48:23 +0100

    completed the addEmployee function and generating a random and unique ID for added employee as well as displaying employees

--- LAB10/projekt/main.cpp
+++ LAB10/projekt/main.cpp
@@ -3,10 +3,12 @@
 #include <random>
 using namespace std;
 
+// For random ID generation
 random_device rd;
 mt19937 gen(rd());
-uniform_int_distribution<int> num(1,9);
+uniform_int_distribution<int> IDgen(1000,9999);
 
+// Decleration of structs
 struct Adress {
     string street = "";
     int houseNumber = 0;
@@ -28,7 +30,9 @@ struct Employee {
     Employee* prev = nullptr;
 };
 
+// List head
 Employee* head = nullptr;
+Employee* lastAdded = nullptr;
 
 Adress* addAdress(string street, int houseNumber, int postalCode, string city, int phoneNumber) {
     Adress* newAdress = new Adress();
@@ -41,27 +45,80 @@ Adress* addAdress(string street, int houseNumber, int postalCode, string city, i
     return newAdress;
 }
 
-void addEmployee(string name, string surname, long pesel, Adress adress, long phoneNumber) {
+bool idExists(long value) {
+    Employee* temp = head;
+    while (temp) {
+        if (temp->ID == value) return true;
+        temp = temp->next;
+    }
+    return false;
+}
+
+long generateRandomID() {
+    Employee* temp = head;
+    long ID;
+
+    do {
+        ID = IDgen(gen);
+    } while (idExists(ID));
+
+    return ID;
+}
+
+void addEmployee(string name, string surname, long pesel, long phoneNumber, Adress* adress) {
     Employee* newEmployee = new Employee();
     newEmployee->name = name;
     newEmployee->surname = surname;
     newEmployee->pesel = pesel;
-    newEmployee->adress = adress;
+    newEmployee->adress = *adress;
     newEmployee->phoneNumber = phoneNumber;
     newEmployee->next = nullptr;
     newEmployee->prev = nullptr;
 
-    string ID = "";
-    for (int i = 0; i < 4; i++) {
-        ID += to_string(num(gen));
-    }
-    long lID = stol(ID);
+    newEmployee->ID = generateRandomID();
 
-    newEmployee->ID = lID;
+    if (head == nullptr) {
+        head = newEmployee;
+        lastAdded = newEmployee;
+    } else {
+        lastAdded->next = newEmployee;
+        newEmployee->prev = lastAdded;
+        lastAdded = newEmployee;
+    }
+}
+
+void displayEmployees() {
+    Employee* temp = head;
+    if (temp == nullptr) {
+        cout << "List is empty..." << endl;
+    }
+    
+    int i = 1;
+    while (temp != nullptr) {
+        cout << "#" << i << ' ';
+        cout << temp->ID << ' ';
+        cout << temp->name << ' ';
+        cout << temp->surname;
+        cout << " Pesel: " << temp->pesel << endl;
+
+        cout << "Adress: ";
+        cout << temp->adress.street << ' ';
+        cout << temp->adress.houseNumber << ' ';
+        cout << temp->adress.city << ' ';
+        cout << temp->adress.phoneNumber << endl;
+
+        cout << temp->position << ' ';
+        cout << temp->salary << endl;
+        cout << "************************************" << endl;
+        
+        temp = temp->next;
+        i++;
+    }
 }
 
 int main() {
-    
+    addEmployee("John", "Pork", 123456789, 123123123, addAdress("main", 12, 12451, "somecity", 123123123));
+    displayEmployees();
 
     return 0;
 }
