dd8a8663b927d4f66485b607e11020aa20ed9f77
Maciej Grześkowiak <maciej.grzes07@gmail.com>
2025-12-16 11:37:04 +0100

    Zadanie 2

---
+++ LAB11/zadanie2.cpp
@@ -0,0 +1,28 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ifstream file("zadanie2.txt");
+
+    if (!file) {
+        cout << "No such file..." << endl;
+        return 1;
+    }
+
+    int arr[10] = {0};
+    int i = 0;
+
+    while (file >> arr[i] && i < 10) {
+        i++;
+    }
+
+    file.close();
+
+    for (int num : arr) {
+        cout << num << ' ';
+    } cout << endl;
+
+    return 0;
+}
